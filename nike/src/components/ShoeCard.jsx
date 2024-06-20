b89b84b15a803381c648bbf67dde9c95eb779fa6
developer-tamim <tawsiftamim24@gmail.com>
2024-06-20 17:20:05 +0600

    nav & hero serction

---
+++ nike/src/components/ShoeCard.jsx
@@ -0,0 +1,19 @@
+const ShoeCard = (imgURL, changeBigShoeImage, bigShoeImg) => {
+    const handleClick = () => {
+        if(bigShoeImg !== imgURL.bigShoe){
+            changeBigShoeImage(imgURL.bigShoe)
+        }
+    }
+  return (
+    <div className="{`border-2 rounded-xl ${bigShoeImage === imgURL ? 'border-coral-red' : 'border-transparent'} cursor-pointer max-sm:flex-1`}" onClick={handleClick}>
+        <div>
+            <img
+                src={imgURL.thumbnail}
+                alt=""
+            />
+        </div>
+    </div>
+  )
+}
+
+export default ShoeCard
