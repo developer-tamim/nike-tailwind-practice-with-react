d6bcbb9d25862c8fb7d64644ae1c241afcd77a55
developer-tamim <tawsiftamim24@gmail.com>
2024-06-20 23:27:30 +0600

    complete hero serction

--- nike/src/sections/Hero.jsx
+++ nike/src/sections/Hero.jsx
@@ -3,9 +3,11 @@ import { bigShoe1 } from "../assets/images"
 import Button from "../components/Button"
 import ShoeCard from "../components/ShoeCard"
 import { shoes, statistics } from "../constants"
+import { useState } from "react"
 
 
 const Hero = () => {
+  const [bigShoeImg, setbigShoeImg] = useState(bigShoe1);
   return (
     <section 
       id="home" 
@@ -34,19 +36,19 @@ const Hero = () => {
 
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
-          src={bigShoe1}
+          src={bigShoeImg}
           alt="shoe collection"
           width={610}
           height={500}
           className="object-contain relative z-10"
         />
-        <div>
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) => (
             <div key={shoe}>
               <ShoeCard
                 imgURL={shoe}
-                changeBigShoeImage = {() => {}}
-                bigShoeImg=""
+                changeBigShoeImage = {(shoe) => setbigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
               />
             </div>
           ))}
